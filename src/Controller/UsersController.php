8fceb10cca4314147fa42994c0c950f71b1be730
xxipacima <xipacima@gmail.com>
2019-05-17 14:52:05 +0300

    setup

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use App\Entity\Users;
+use App\Form\UserAdd;
+
+
+class UsersController extends AbstractFOSRestController
+{
+    /**
+     * @Rest\Get("/users", name="users")
+     */
+
+    public function getAction()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UsersController.php',
+        ]);
+    }
+
+    /**
+     * Create User.
+     * @Rest\Post("/user")
+     *
+     * @return array
+     */
+    public function postUserAction(Request $request)
+    {
+        $user = new Users();
+        $user->setFirstName($request->get('firstName'));
+        $user->setLastName($request->get('lastName'));
+        $phoneNumbers[] = $request->get('phoneNumbers');
+        foreach ($phoneNumbers as $phoneNumber) {
+            $user->setPhoneNumbers($phoneNumber);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        return View::create($user, Response::HTTP_CREATED , []);
+
+    }
+}
---
+++ src/Entity/Users.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $phoneNumbers = [];
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $actual;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getPhoneNumbers(): ?array
+    {
+        return $this->phoneNumbers;
+    }
+
+    public function setPhoneNumbers(string $phoneNumbers): self
+    {
+        $this->phoneNumbers[] = $phoneNumbers;
+
+        return $this;
+    }
+
+    public function getActual(): ?bool
+    {
+        return $this->actual;
+    }
+
+    public function setActual(bool $actual): self
+    {
+        $this->actual = $actual;
+
+        return $this;
+    }
+}
